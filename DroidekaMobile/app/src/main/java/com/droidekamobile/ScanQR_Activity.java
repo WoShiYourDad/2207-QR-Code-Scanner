59b7bcc7cb3aa38eb1285846c7192ee85c96f9c0
“Jun <2101622@sit.singaporetech.edu.sg>
2023-01-23 22:45:15 +0800

    qr scanning interface without permission

--- DroidekaMobile/app/src/main/java/com/droidekamobile/ScanQR_Activity.java
+++ DroidekaMobile/app/src/main/java/com/droidekamobile/ScanQR_Activity.java
@@ -1,14 +1,60 @@
 package com.droidekamobile;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import com.budiyev.android.codescanner.CodeScanner;
+import com.budiyev.android.codescanner.CodeScannerView;
+import com.budiyev.android.codescanner.DecodeCallback;
+import com.google.zxing.Result;
+
 
 public class ScanQR_Activity extends AppCompatActivity {
+    private CodeScanner mCodeScanner;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_scan_qr);
+
+        //Bind variables to element ID on screen
+        CodeScannerView scannerView = findViewById(R.id.scanner_view);
+
+        mCodeScanner = new CodeScanner(this, scannerView);
+        mCodeScanner.setDecodeCallback(new DecodeCallback() {
+            @Override
+            public void onDecoded(@NonNull final Result result) {
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        Toast.makeText(ScanQR_Activity.this, result.getText(), Toast.LENGTH_SHORT).show();
+                    }
+                });
+            }
+        });
+
+        //Create Listeners to perform action onClick for the different buttons
+        scannerView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mCodeScanner.startPreview();
+            }
+        });
     }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        mCodeScanner.startPreview();
+    }
+
+    @Override
+    protected void onPause() {
+        mCodeScanner.releaseResources();
+        super.onPause();
+    }
+
 }
