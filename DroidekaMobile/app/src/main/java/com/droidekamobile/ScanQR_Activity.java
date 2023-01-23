b04becc713cf3c6f71cc6d45d0867cc53e215c8a
“Jun <2101622@sit.singaporetech.edu.sg>
2023-01-24 00:21:26 +0800

    ask for permission to use camera

--- DroidekaMobile/app/src/main/java/com/droidekamobile/ScanQR_Activity.java
+++ DroidekaMobile/app/src/main/java/com/droidekamobile/ScanQR_Activity.java
@@ -3,10 +3,15 @@ package com.droidekamobile;
 import androidx.activity.result.ActivityResultLauncher;
 import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
 import android.Manifest;
+import android.content.DialogInterface;
+import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Toast;
@@ -20,37 +25,43 @@ import com.google.zxing.Result;
 
 public class ScanQR_Activity extends AppCompatActivity {
     private CodeScanner mCodeScanner;
-    private static final int CAMERA_REQUEST_CODE = 101;
+    private static final int PERMISSION_REQUEST_CODE = 200;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_scan_qr);
 
-        //Bind variables to element ID on screen
-        CodeScannerView scannerView = findViewById(R.id.scanner_view);
+        // Check if camera permission is permitted, if so then run the main code
+        if (checkPermission()) {
+            //Bind variables to element ID on screen
+            CodeScannerView scannerView = findViewById(R.id.scanner_view);
 
-        mCodeScanner = new CodeScanner(this, scannerView);
-        mCodeScanner.setScanMode(ScanMode.CONTINUOUS);
-        mCodeScanner.setDecodeCallback(new DecodeCallback() {
-            @Override
-            public void onDecoded(@NonNull final Result result) {
-                runOnUiThread(new Runnable() {
-                    @Override
-                    public void run() {
-                        Toast.makeText(ScanQR_Activity.this, result.getText(), Toast.LENGTH_SHORT).show();
-                    }
-                });
-            }
-        });
+            mCodeScanner = new CodeScanner(this, scannerView);
+            mCodeScanner.setScanMode(ScanMode.CONTINUOUS);
+            mCodeScanner.setDecodeCallback(new DecodeCallback() {
+                @Override
+                public void onDecoded(@NonNull final Result result) {
+                    runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            Toast.makeText(ScanQR_Activity.this, result.getText(), Toast.LENGTH_SHORT).show();
+                        }
+                    });
+                }
+            });
 
-        //Create Listeners to perform action onClick for the different buttons
-        scannerView.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                mCodeScanner.startPreview();
-            }
-        });
+            //Create Listeners to perform action onClick for the different buttons
+            scannerView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    mCodeScanner.startPreview();
+                }
+            });
+        } else { //if camera permission is not permitted
+            requestPermission();
+        }
+
     }
 
     @Override
@@ -65,4 +76,56 @@ public class ScanQR_Activity extends AppCompatActivity {
         super.onPause();
     }
 
+    private boolean checkPermission() {
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
+            // Permission is not granted
+            return false;
+        }
+        return true;
+    }
+
+    private void requestPermission() {
+
+        ActivityCompat.requestPermissions(this,
+                new String[]{Manifest.permission.CAMERA},
+                PERMISSION_REQUEST_CODE);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String permissions[], int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        switch (requestCode) {
+            case PERMISSION_REQUEST_CODE:
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    Toast.makeText(getApplicationContext(), "Permission Granted", Toast.LENGTH_SHORT).show();
+
+                    // main logic
+                } else {
+                    Toast.makeText(getApplicationContext(), "Permission Denied", Toast.LENGTH_SHORT).show();
+                    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                        if (ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
+                            showMessageOKCancel("You need to allow access permissions",
+                                    new DialogInterface.OnClickListener() {
+                                        @Override
+                                        public void onClick(DialogInterface dialog, int which) {
+                                            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                                                requestPermission();
+                                            }
+                                        }
+                                    });
+                        }
+                    }
+                }
+                break;
+        }
+    }
+
+    private void showMessageOKCancel(String message, DialogInterface.OnClickListener okListener) {
+        new AlertDialog.Builder(ScanQR_Activity.this)
+                .setMessage(message)
+                .setPositiveButton("OK", okListener)
+                .setNegativeButton("Cancel", null)
+                .create()
+                .show();
+    }
 }
