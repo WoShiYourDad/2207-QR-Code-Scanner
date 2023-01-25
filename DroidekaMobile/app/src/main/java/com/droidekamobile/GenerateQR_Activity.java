bd9c30debdb48001713df276e11a6d5aeb03c0ec
Elysia <elysiayeoje@gmail.com>
2023-01-26 00:36:20 +0800

    added share feature for generate qr page

--- DroidekaMobile/app/src/main/java/com/droidekamobile/GenerateQR_Activity.java
+++ DroidekaMobile/app/src/main/java/com/droidekamobile/GenerateQR_Activity.java
@@ -1,10 +1,20 @@
 package com.droidekamobile;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+import androidx.core.content.FileProvider;
 
+import android.Manifest;
+import android.content.DialogInterface;
+import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
 import android.graphics.Color;
 import android.graphics.Point;
+import android.graphics.drawable.BitmapDrawable;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.Display;
 import android.view.View;
@@ -13,10 +23,13 @@ import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
+import android.content.Intent;
 
 import com.google.android.material.textfield.TextInputEditText;
 import com.google.zxing.WriterException;
 
+import java.io.File;
+import java.io.FileOutputStream;
 import java.io.Writer;
 
 import androidmads.library.qrgenearator.QRGContents;
@@ -31,6 +44,8 @@ public class GenerateQR_Activity extends AppCompatActivity {
     private Button generateQRBtn;
     private QRGEncoder qrgEncoder;
     private Bitmap bitmap;
+    private Button shareBtn;
+    public static final int PERMISSIONS_REQUEST_READ_CONTACTS = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -42,6 +57,7 @@ public class GenerateQR_Activity extends AppCompatActivity {
         qrCodeIV = findViewById(R.id.idIVQRCode);
         dataEdit = findViewById(R.id.idEditData);
         generateQRBtn = findViewById(R.id.idBtnGenerateQR);
+        shareBtn = findViewById(R.id.share_new_qr);
 
         //Create Listeners to perform action onClick for the different buttons
         generateQRBtn.setOnClickListener(new View.OnClickListener(){
@@ -66,8 +82,105 @@ public class GenerateQR_Activity extends AppCompatActivity {
                     bitmap = qrgEncoder.getBitmap();
                     qrCodeTV.setVisibility(View.GONE);
                     qrCodeIV.setImageBitmap(bitmap);
+
+                    shareBtn.setVisibility(View.VISIBLE);
                 }
             }
         });
+
+        shareBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                BitmapDrawable bitmapDrawable = (BitmapDrawable) qrCodeIV.getDrawable();
+                Bitmap bitmap = bitmapDrawable.getBitmap();
+                shareImage(bitmap);
+            }
+        });
+    }
+
+    public void shareImage(Bitmap bitmap){
+        // Check if contact permission is granted, if so then run
+        if (checkContactPermission()) {
+            Uri uri = getImageToShare(bitmap);
+            Intent sendIntent = new Intent();
+            sendIntent.setAction(Intent.ACTION_SEND);
+            sendIntent.putExtra(Intent.EXTRA_STREAM, uri);
+            sendIntent.setType("image/png");
+            Intent shareIntent = Intent.createChooser(sendIntent, null);
+            startActivity(shareIntent);
+        }
+        else{
+            requestContactPermission();
+        }
+    }
+
+    private Uri getImageToShare(Bitmap bitmap) {
+        File imagefolder = new File(getCacheDir(), "images");
+        Uri uri = null;
+        try {
+            imagefolder.mkdirs();
+            File file = new File(imagefolder, "qr.png");
+            FileOutputStream outputStream = new FileOutputStream(file);
+            bitmap.compress(Bitmap.CompressFormat.PNG, 90, outputStream);
+            outputStream.flush();
+            outputStream.close();
+            uri = FileProvider.getUriForFile(this, "com.anni.shareimage.fileprovider", file);
+        } catch (Exception e) {
+            Toast.makeText(this, "" + e.getMessage(), Toast.LENGTH_LONG).show();
+        }
+        return uri;
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String permissions[], int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        switch (requestCode) {
+            case PERMISSIONS_REQUEST_READ_CONTACTS:
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    Toast.makeText(getApplicationContext(), "Permission Granted", Toast.LENGTH_SHORT).show();
+
+                    // main logic
+                } else {
+                    Toast.makeText(getApplicationContext(), "Permission Denied", Toast.LENGTH_SHORT).show();
+                    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                        if (ContextCompat.checkSelfPermission(this, Manifest.permission.READ_CONTACTS) != PackageManager.PERMISSION_GRANTED) {
+                            showMessageOKCancel("You need to allow access permissions",
+                                    new DialogInterface.OnClickListener() {
+                                        @Override
+                                        public void onClick(DialogInterface dialog, int which) {
+                                            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                                                requestContactPermission();
+                                            }
+                                        }
+                                    });
+                        }
+                    }
+                }
+                break;
+        }
+    }
+
+    private void showMessageOKCancel(String message, DialogInterface.OnClickListener okListener) {
+        new AlertDialog.Builder(GenerateQR_Activity.this)
+                .setMessage(message)
+                .setPositiveButton("OK", okListener)
+                .setNegativeButton("Cancel", null)
+                .create()
+                .show();
+    }
+
+    private boolean checkContactPermission() {
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.READ_CONTACTS) != PackageManager.PERMISSION_GRANTED) {
+            // Permission is not granted
+            return false;
+        }
+        return true;
+    }
+
+    private void requestContactPermission() {
+
+        ActivityCompat.requestPermissions(this,
+                new String[]{Manifest.permission.READ_CONTACTS},
+                PERMISSIONS_REQUEST_READ_CONTACTS);
     }
 }
