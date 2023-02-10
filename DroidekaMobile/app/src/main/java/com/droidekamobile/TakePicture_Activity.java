5197c96ae68b216fd56c1629caf64daaa624efb3
ministic2001 <kehxuan@gmail.com>
2023-02-11 00:17:30 +0800

    Uploaded photo to database works sometimes?

--- DroidekaMobile/app/src/main/java/com/droidekamobile/TakePicture_Activity.java
+++ DroidekaMobile/app/src/main/java/com/droidekamobile/TakePicture_Activity.java
@@ -1,5 +1,7 @@
 package com.droidekamobile;
 
+import static android.content.ContentValues.TAG;
+
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.camera.core.CameraSelector;
@@ -21,18 +23,34 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.common.util.concurrent.ListenableFuture;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
 
+import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.IOException;
+import java.util.UUID;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.Executor;
 
 public class TakePicture_Activity extends AppCompatActivity{
     private ListenableFuture<ProcessCameraProvider> cameraProviderFuture;
     PreviewView previewView;
+    private static FirebaseStorage mStorage = FirebaseStorage.getInstance();
+    private static DatabaseReference mDatabase = FirebaseDatabase.getInstance().getReference();
+    private static FirebaseAuth mAuth = FirebaseAuth.getInstance();
+
 
     private ImageCapture imageCapture;
 
@@ -103,18 +121,21 @@ public class TakePicture_Activity extends AppCompatActivity{
                     @Override
                     public void onImageSaved(@NonNull ImageCapture.OutputFileResults outputFileResults) {
                         Uri uri = outputFileResults.getSavedUri();
-                        String uriStr = "content://media" + uri.toString();
-                        Uri newUri = Uri.parse(uriStr);
+
                         Bitmap bitmap = null; // https://www.geeksforgeeks.org/android-how-to-upload-an-image-on-firebase-storage/
                         try {
-                            bitmap = MediaStore.Images.Media.getBitmap(getContentResolver(), newUri);
+                            bitmap = MediaStore.Images.Media.getBitmap(getContentResolver(), uri);
+                            sendImage(bitmap);
+                            Log.d("HERE2", "HERE2");
                         } catch (IOException e) {
                             e.printStackTrace();
+                            Log.d("HERE3", "HERE3");
                         }
 
-                        Toast.makeText(TakePicture_Activity.this,"file uri " + newUri,Toast.LENGTH_LONG).show();
+                        Toast.makeText(TakePicture_Activity.this,"file uri " + uri,Toast.LENGTH_LONG).show();
 //                        Toast.makeText(TakePicture_Activity.this,"file path " + uri.getPath(),Toast.LENGTH_LONG).show();
 //                        Toast.makeText(TakePicture_Activity.this,"Saving...",Toast.LENGTH_SHORT).show();
+
                         finish();
                     }
 
@@ -129,7 +150,67 @@ public class TakePicture_Activity extends AppCompatActivity{
     private void deletePic() {
 
     }
-    public void sendImage(Uri uri) {
 
+    public void sendImage(Bitmap bitmap) { //https://stackoverflow.com/questions/40885860/how-to-save-bitmap-to-firebase
+        obtainUsername(username -> {
+            StorageReference imageRef = mStorage.getReference().child(username).child("Images").child("images/"+ UUID.randomUUID().toString());
+            Log.d("HERE3", "HERE3");
+            ByteArrayOutputStream baos = new ByteArrayOutputStream();
+            bitmap.compress(Bitmap.CompressFormat.PNG, 100, baos);
+            byte[] data = baos.toByteArray();
+
+            UploadTask uploadTask = imageRef.putBytes(data);
+            uploadTask.addOnFailureListener(new OnFailureListener() {
+                @Override
+                public void onFailure(@NonNull Exception exception) {
+                    // Handle unsuccessful uploads
+                    Log.d("HEREFAIL", "HEREFAIL");
+                }
+            }).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                @Override
+                public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                    // taskSnapshot.getMetadata() contains file metadata such as size, content-type, and download URL.
+                    Uri downloadUrl = Uri.parse(taskSnapshot.getMetadata().getReference().getDownloadUrl().toString()); //https://stackoverflow.com/questions/50585334/tasksnapshot-getdownloadurl-method-not-working
+                    Log.d("HERE", downloadUrl.toString());
+                }
+            });
+        });
+    }
+
+    private void obtainUsername(FirebaseCallback firebaseCallback) {
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        String email = currentUser.getEmail();
+        mDatabase = FirebaseDatabase.getInstance().getReference("users");
+
+        // Incase you do not understand this part: https://www.youtube.com/watch?v=OvDZVV5CbQg
+        // This solves the asynchronous issue
+        ValueEventListener valueEventListener = new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+
+                for (DataSnapshot dataSnapshot : snapshot.getChildren()) {
+                    User user = dataSnapshot.getValue(User.class);
+                    if (user.getEmail().equals(email)) {
+                        String username = user.getUsername();
+                        firebaseCallback.onCallback(username);
+                        return;
+                    }
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+                Log.d(TAG, error.getMessage());
+                return;
+            }
+        };
+        mDatabase.addListenerForSingleValueEvent(valueEventListener);
+
+        return;
+    }
+
+    //This is just needed to fight against the asynchronous issue when obtaining data from database
+    private interface FirebaseCallback {
+        void onCallback(String username);
     }
 }
